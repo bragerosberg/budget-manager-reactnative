ee1e2f1e21df8566782d6a4dc90e731327f71110
Brage Rosberg <brage70@gmail.com>
2020-09-24 17:49:00 +0200

    Budget input setup complete. Add a numeric value, when budget set it takes you forward to the next step

--- components/App.js
+++ components/App.js
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import Board from './board/Board';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Board />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/board/Board.jsx
+++ components/board/Board.jsx
@@ -1,13 +1,72 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, AsyncStorage, TextInput } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 import Budget from '../budget/Budget';
 
 export default function Board() {
+
+  const [yearlyBudget, updateYearlyBudget] = useState("");
+  const [budgetSet, setBudgetStatus] = useState(false);
+  let attemptSavedBudget;
   
-  return (
-    <View>
-      <Text>This is the Board component</Text>
+  _retrieveData = async () => {
+    try {
+      attemptSavedBudget = await AsyncStorage.getItem('budget');
+      if (attemptSavedBudget !== null) {
+        console.log(attemptSavedBudget);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  // useEffect(() => {
+  //   if(attemptSavedBudget !== "" || attemptSavedBudget === undefined) setBudgetStatus(false);
+  // }, [attemptSavedBudget ])
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { value } = target;
+    console.log('I am run');
+    updateYearlyBudget(value);
+  };
+
+  const resetBudget = () => {
+    handleSubmit();
+    AsyncStorage.clear();
+    updateYearlyBudget("");
+  }
+
+  const handleSubmit = (e) => {
+    if (e) e.preventDefault();
+    if(yearlyBudget !== "") {
+      AsyncStorage.setItem('budget');
+      setBudgetStatus(budgetSet => ! budgetSet);
+    } else {
+      alert('Invalid budget, please enter a valid number');
+    }
+  };
+  
+  return budgetSet ? (
+    <View style={{marginTop: 50}}>
+      <Text style={{textAlign: "center" }}>Yearly: {yearlyBudget}</Text>
       <Budget />
+    </View>
+  ) : (
+    <View style={{marginTop: 50}}>
+      <Text style={{marginTop: 10, textAlign: "center", fontSize: 32}}>Budget Manager</Text>
+      <TextInput 
+        placeholder="Enter yearly budget here"
+        keyboardType = 'number-pad'
+        onChangeText={updateYearlyBudget}
+        inputStyle={{ color: 'black' }}
+        style={{textAlign: "center"}}
+      />
+      <Button
+        title="Submit"
+        onPress={handleSubmit}
+        buttonStyle={{backgroundColor: "green"}}
+      />
     </View>
   );
 }
