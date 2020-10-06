55660523ef153c68026c6b26c0843544973e7e6f
Brage Rosberg <brage70@gmail.com>
2020-10-06 15:16:47 +0200

    Gathered styling on the Expense component and seperated them into a stylesheet for better scalability and maintenance

--- components/expense/Expense.jsx
+++ components/expense/Expense.jsx
@@ -1,31 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Button } from 'react-native-elements';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function Expense(props) {
   const [toggleRemove, toggleRemoveStatus] = useState(false);
   const handleEditClick = () => toggleRemoveStatus(!toggleRemove);
 
   return toggleRemove ? (
-    <View style={{ flex: 1, justifyContent: "center", flexDirection: "row" }}>
-      <Text style={{margin: 8}}>{props.exp.name}</Text>
-      <Text style={{margin: 8}}> - </Text>
-      <Text style={{color: "#658d28", margin: 8 }}>{props.exp.amount}</Text>
+    <View style={styles.monthExpenseEntry}>
+      <Text style={styles.monthText}>{props.exp.name}</Text>
+      <Text style={styles.monthText}> - </Text>
+      <Text style={styles.monthAmount}>{props.exp.amount}</Text>
       <TouchableOpacity onPress={handleEditClick}>
-        <Text style={{margin: 8}}>Edit</Text>
+        <Text style={styles.monthText}>Edit</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => props.deleteExpense(props.exp.id)} name={props.exp.name} id={props.exp.id}>
-        <Text style={{margin: 8, backgroundColor: "red", padding: 10, color: "white"}}>X</Text>
+        <Text style={styles.monthDelete}>X</Text>
       </TouchableOpacity>
     </View>
   ) : (
-    <View style={{ flex: 1, justifyContent: "center", flexDirection: "row" }}>
-      <Text style={{margin: 8}}>{props.exp.name}</Text>
-      <Text style={{margin: 8}}> - </Text>
-      <Text style={{color: "#658d28", margin: 8 }}>${props.exp.amount}</Text>
+    <View style={styles.monthExpenseEntry}>
+      <Text style={styles.monthText}>{props.exp.name}</Text>
+      <Text style={styles.monthText}> - </Text>
+      <Text style={ styles.monthAmount }>${props.exp.amount}</Text>
       <TouchableOpacity onPress={handleEditClick}>
-        <Text style={{margin: 8}}>Edit</Text>
+        <Text style={styles.monthText}>Edit</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  monthExpenseEntry: {
+    flex: 1,
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  monthText: {
+    margin: 8
+  },
+  monthAmount: {
+    margin: 8,
+    color: "#658d28"
+  },
+  monthDelete: {
+    margin: 8,
+    backgroundColor: "red",
+    padding: 10,
+    color: "white"
+  },
+});
